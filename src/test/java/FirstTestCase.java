ca9b170c2bdbcba9d90a542a605f1c3d19cf3cee
enfl333 <enrique.pflores@gmail.com>
2021-03-15 22:15:56 +0100

    Added TestNg support, priority annotaion

---
+++ src/test/java/FirstTestCase.java
@@ -0,0 +1,34 @@
+import org.testng.annotations.Test;
+
+
+/***
+ * Get the maven dependency working intellij Run working and run
+ * the test in Priority order
+ *
+ * @author enrique
+ *
+ */
+public class FirstTestCase {
+
+    @Test(priority = 1)
+    public void testOne(){
+        System.out.println("TestNg nr 1");
+    }
+
+    @Test(priority = 2)
+    public void testTwo(){
+        System.out.println("TestNg nr 2");
+    }
+
+    @Test(priority = 3)
+    public void testThree(){
+        System.out.println("TestNg nr 3");
+    }
+
+    @Test(priority = 4)
+    public void testFour(){
+        System.out.println("TestNg nr 4");
+    }
+
+
+}
