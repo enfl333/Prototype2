5b3a9252e20137a37a874f7c07dad1dbbb499966
enfl333 <enrique.pflores@gmail.com>
2021-03-29 22:02:27 +0200

    Selium driver and setup in maven to develop SeleniumTest for TestNg selenium tutorial

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/***
+ * Assertions in TestNg framework
+ */
+public class SeleniumTest {
+
+    WebDriver driver;
+    @BeforeTest
+    void setUp(){
+        System.out.println("Set up");
+        System.setProperty("webdriver.chrome.driver","C:/WebDrivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com");
+    }
+
+    @Test(priority = 1)
+    void logoTest(){
+        WebElement webElement = driver.findElement(By.xpath("//*[@id=\"divLogo\"]/img"));
+        Assert.assertTrue(webElement.isDisplayed(),"Logo not displayed");
+    }
+
+    @Test(priority = 2)
+    void homePageTitle(){
+        String title =  driver.getTitle();
+        Assert.assertEquals("OrangeHRM",title,"Title is not matched");
+    }
+
+    @AfterClass
+    void tearDown(){
+        driver.quit();
+    }
+
+
+}
